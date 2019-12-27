6bedec73f63eb142cb05fd3214ddb48093da46ca
Your Name <you@example.com>
2019-12-27 12:57:02 +0700

    update commit

---
+++ module1/nhapmonlaptrinh/bai-tap/7_ungdungmaytinh/duc123.js
@@ -0,0 +1,29 @@
+
+function cong() {
+    let  number = document.getElementById("number").value;
+    let number_1 = document.getElementById("number_1").value;
+
+
+ document.write(parseInt(number) + parseInt(number_1));}
+
+
+ function tru() {
+
+     let  number = document.getElementById("number").value;
+     let number_1 = document.getElementById("number_1").value;
+     document.write(parseInt(number) - parseInt(number_1));
+}
+
+function nhan() {
+
+    let  number = document.getElementById("number").value;
+    let number_1 = document.getElementById("number_1").value;
+    document.write(parseInt(number) * parseInt(number_1));
+}
+
+function chia() {
+
+    let  number = document.getElementById("number").value;
+    let number_1 = document.getElementById("number_1").value;
+    document.write(parseInt(number) / parseInt(number_1));
+}
